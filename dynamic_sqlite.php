39b11e93585ff9cafa79b310d56699ecca4e3fa6
Christian Tang <christian@tangc.dk>
2014-01-26 23:35:14 +0100

    Code for inserting
    PHP function for inserting an array into a SQLite table.

---
+++ dynamic_sqlite.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @author Christian Tang
+ * @copyright 2014
+ */
+
+function sqlite_insert($db, $table, $array)
+{
+    try 
+    {
+        $file_db = new PDO('sqlite:'.$db);
+        $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        foreach($array as $k => $v)
+        {
+            $colums .= $k.',';
+            $values .= ':'.$k.',';
+        }
+        $insert = 'INSERT INTO '.$table.' ('.rtrim($colums, ',').') VALUES('.rtrim($values, ',').')';
+        
+        $stmt = $file_db->prepare($insert);
+        foreach($array as $k => $v)
+            $stmt->bindValue(':'.$k, $v);
+            
+        return $stmt->execute();
+    }
+    catch(PDOException $e)
+    {
+        die($e->getMessage());
+    }
+}
+?>
